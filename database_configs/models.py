d8f817e80d665f1d09b863b94199ef72cf1ad010
Jack Riley <jaackriiley@gmail.com>
2023-07-18 23:56:42 +0100

    Created a pokemon class to help populate my database

---
+++ database_configs/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from database_configs import Base
+
+class Pokemon(Base):
+    __tablename__ = "pokemon"
+    number = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    type1 = Column(String)
+    type2 = Column(String)
+    generation = Column(Integer)
+    legendary = Column(Boolean)
