b463ee9166cc4d9bff5c58c9abdf00b47e384cee
Jack Riley <jaackriiley@gmail.com>
2023-07-19 22:53:58 +0100

    fixed the error that was causing the Base class ImportError

--- database_configs/models.py
+++ database_configs/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from database_configs import Base
+from database_configs.connection import Base
 
 class Pokemon(Base):
     __tablename__ = "pokemon"
