43216237db5af824d004a5d8adb6840faad0c10f
Jack Riley <jaackriiley@gmail.com>
2023-07-19 23:15:38 +0100

    Added code to load the data from the csv file into the database but encountered erro when runnning into duplicate in '#' column

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from pokemon_logic import PokemonService
-from database_configs.connection import engine, Base
+from database_configs.connection import engine, Base, SessionLocal
 from database_configs import models
+from parse_data import parse_data
 
 Base.metadata.create_all(bind=engine)
 
 
 #Setting up the FastAPI object
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    db = SessionLocal()
+    parse_data(db)
+    db.close()
 
 #Setting up home route for API requests
 @app.get("/")
