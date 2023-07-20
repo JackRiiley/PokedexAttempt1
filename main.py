546ea95c2fd84528a71ddda8f4a35c7c0abbb57f
Jack Riley <jaackriiley@gmail.com>
2023-07-20 20:50:48 +0100

    Tidied up main.py and imported everything i think i need to get the endpoints working

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from fastapi import FastAPI
 from pokemon_logic import PokemonService
-from database_configs.connection import engine, Base, SessionLocal
+from sqlalchemy.orm import Session, Depends
+from database_configs.connection import engine, Base, SessionLocal, get_db
 from database_configs import models
-from parse_data import parse_data
+from parse_data import parse_pokemon, parse_stats
 
 Base.metadata.create_all(bind=engine)
 
 
 #Setting up the FastAPI object
 app = FastAPI()
-
-@app.on_event("startup")
-def startup_event():
-    db = SessionLocal()
-    parse_data(db)
-    db.close()
 
 #Setting up home route for API requests
 @app.get("/")
@@ -23,9 +18,8 @@ def home():
 
 #Creating an API endpoint for all pokemon
 @app.get("/pokemon")
-def GetAllPokemon():
-    pokemonService = PokemonService()
-    return pokemonService.GetAllPokemon()
+def GetAllPokemon(db: Session = Depends(get_db)):
+    return
 
 #Creating an API endpoint for searching for a pokemon
 @app.get("/pokemon/{pokemon_name}")
