e8618a0e4e89dd618391437d9ae2c2377bcdba1a
Jack Riley <jaackriiley@gmail.com>
2023-07-20 20:47:38 +0100

    Added another function in parse_data to obtain the stats from the .ccsv file and put them into a table

--- parse_data.py
+++ parse_data.py
@@ -1,8 +1,8 @@
 import csv
 from sqlalchemy.orm import Session
-from database_configs.models import Pokemon
+from database_configs.models import Pokemon, PokemonStats
 
-def parse_data(db: Session):
+def parse_pokemon(db: Session):
     csv_file = "./Pokemon.csv"
     
     #Opens the CSV file and reads it
@@ -18,7 +18,34 @@ def parse_data(db: Session):
             legendary = row["Legendary"] == "True"
             
             #create instance of Pokemon Model and populate data
-            pokemon = Pokemon(number=number, name=name, type1=type1, type2=type2, generation=generation)
+            pokemon = Pokemon(number=number, name=name, type1=type1, type2=type2, generation=generation, legendary=legendary)
             
             db.add(pokemon)
+            
             db.commit()
+            db.close()
+            
+def parse_stats(db: Session):
+    csv_file = "./Pokemon.csv"
+    
+    #Opens the CSV file and reads it
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            total = int(row["Total"])
+            hp = int(row["HP"])
+            attack = int(row["Attack"])
+            defense = int(row["Defense"])
+            sp_atk = int(row["Sp. Atk"])
+            sp_def = int(row["Sp. Def"])
+            speed = int(row["Speed"])
+            
+            
+            #create instance of Pokemon Model and populate data
+            pokemonStats = PokemonStats(total=total, hp=hp, attack=attack, defense=defense, sp_atk=sp_atk, sp_def=sp_def, speed=speed)
+            
+            db.add(pokemonStats)
+            
+            db.commit()
+            db.close()
