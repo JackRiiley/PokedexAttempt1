6090d13306f7a4810b8b6b1ae947209205788c93
Jack Riley <jaackriiley@gmail.com>
2023-07-20 23:30:58 +0100

    Attempting to load information from database

--- pokemon_logic/PokemonService.py
+++ pokemon_logic/PokemonService.py
@@ -1,17 +1,9 @@
-from sqlalchemy.orm import Session, Depends
+from sqlalchemy.orm import Session
 from database_configs.connection import engine, Base, SessionLocal, get_db
 from database_configs.models import Pokemon, PokemonStats
 class PokemonService:
-    def __init__ (self):
-        pass
+    def __init__ (self, db: Session):
+        self.db = db
     
-    def GetAllPokemon(self, db: Session):
-        return db.query(Pokemon).join(PokemonStats, PokemonStats.pokemon_id == Pokemon.id).all()
-    
-    def GetPokemonByName(self, pokemon_name: str):
-        
-        for pokemon in PokemonList:
-            if pokemon["name"].__contains__(pokemon_name.lower()):
-                return pokemon
-        
-        return {"message": "Pokemon not found"}
+    def GetAllPokemon(self):
+        return self.db.query(Pokemon).all()
