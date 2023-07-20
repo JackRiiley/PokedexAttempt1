3f53333292a8646c52bc5378d13fec29635e81f3
Jack Riley <jaackriiley@gmail.com>
2023-07-20 20:48:12 +0100

    Attempting to write a function that returns all pokemon in the table and their associated stats

--- pokemon_logic/PokemonService.py
+++ pokemon_logic/PokemonService.py
@@ -1,10 +1,12 @@
+from sqlalchemy.orm import Session, Depends
+from database_configs.connection import engine, Base, SessionLocal, get_db
+from database_configs.models import Pokemon, PokemonStats
 class PokemonService:
     def __init__ (self):
         pass
     
-    def GetAllPokemon(self):
-    #TO-DO: implement this 
-        return
+    def GetAllPokemon(self, db: Session):
+        return db.query(Pokemon).join(PokemonStats, PokemonStats.pokemon_id == Pokemon.id).all()
     
     def GetPokemonByName(self, pokemon_name: str):
         
