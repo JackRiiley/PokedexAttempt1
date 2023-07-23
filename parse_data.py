e9508d3551073d675dc0d4ff52dac0fa70af95e0
Jack Riley <jaackriiley@gmail.com>
2023-07-23 15:59:30 +0100

    Combined the two parse functions and split them in the same function, thiss solved the foreign key issue i was having

--- parse_data.py
+++ parse_data.py
@@ -2,7 +2,7 @@ import csv
 from sqlalchemy.orm import Session
 from database_configs.models import Pokemon, PokemonStats
 
-def parse_pokemon(db: Session):
+def parse_pokemon_and_stats(db: Session):
     csv_file = "./Pokemon.csv"
     
     #Opens the CSV file and reads it
@@ -16,24 +16,6 @@ def parse_pokemon(db: Session):
             type2 = row["Type 2"]
             generation = int(row["Generation"])
             legendary = row["Legendary"] == "True"
-            
-            #create instance of Pokemon Model and populate data
-            pokemon = Pokemon(number=number, name=name, type1=type1, type2=type2, generation=generation, legendary=legendary)
-            
-            db.add(pokemon)
-            
-        db.commit()
-            
-        db.close()
-            
-def parse_stats(db: Session):
-    csv_file = "./Pokemon.csv"
-    
-    #Opens the CSV file and reads it
-    with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file)
-        
-        for row in reader:
             total = int(row["Total"])
             hp = int(row["HP"])
             attack = int(row["Attack"])
@@ -42,10 +24,14 @@ def parse_stats(db: Session):
             sp_def = int(row["Sp. Def"])
             speed = int(row["Speed"])
             
-            #create instance of Pokemon Model and populate data
-            pokemonStats = PokemonStats(total=total, hp=hp, attack=attack, defense=defense, sp_atk=sp_atk, sp_def=sp_def, speed=speed) 
+            #create an instancec fo the pokemonStats Model
+            pokemon_stats = PokemonStats(total=total, hp=hp, attack=attack, defense=defense, sp_atk=sp_atk, sp_def=sp_def, speed=speed) 
             
-            db.add(pokemonStats)
+            #create instance of Pokemon Model and populate data
+            pokemon = Pokemon(number=number, name=name, type1=type1, type2=type2, generation=generation, legendary=legendary, stats=pokemon_stats)
+            
+            db.add(pokemon)
+            db.add(pokemon_stats)
             
         db.commit()
         db.close()
