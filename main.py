ca87604dea59177e8eba8c27d639e62ca12fa318
Jack Riley <jaackriiley@gmail.com>
2023-07-23 12:03:59 +0100

    added a small startup to load .csv information into table for stats - not working properly so will revisit this

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from sqlalchemy.orm import Session
 from database_configs.connection import engine, Base, SessionLocal, get_db
 from database_configs import models
 from parse_data import parse_pokemon, parse_stats
-from schemas import PokemonSchema
+
 
 Base.metadata.create_all(bind=engine)
 
 
 #Setting up the FastAPI object
 app = FastAPI()
+
+# @app.on_event("startup")
+# def startup_event():
+#     db = SessionLocal()
+#     parse_stats(db)
+#     db.close()
 
 #Setting up home route for API requests
 @app.get("/")
@@ -21,6 +27,10 @@ def home():
 @app.get("/pokemon")
 def GetAllPokemon(db: Session = Depends(get_db)):
     return db.query(models.Pokemon).all()
+
+@app.get("/PokemonStats)")
+def GetAllPokemonStats(db: Session = Depends(get_db)):
+    return db.query(models.PokemonStats).all()
 
 #Creating an API endpoint for searching for a pokemon
 @app.get("/pokemon/{pokemon_name}")
